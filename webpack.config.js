bcf61737af2b06f96ec49d3d0e00d632838c44b4
nkjmsss <nakajima-sota018@g.ecc.u-tokyo.ac.jp>
2019-08-25 12:33:48 +0900

    fix: polyfills

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,27 @@
 const path = require('path')
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'markdown-it-color.min.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  mode: 'production',
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: 'babel-loader',
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development'
+
+  return {
+    entry: './src/index.js',
+    output: {
+      filename: 'markdown-it-color.min.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    mode: isDev ? 'development' : 'production',
+    module: {
+      rules: [
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: [
+            {
+              loader: 'babel-loader',
+            },
+          ],
         },
-      },
-    ],
-  },
+      ],
+    },
+  }
 }
